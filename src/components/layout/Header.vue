71df40f5d231c5ed2b7a750623060dc738d7084b
EomYerim <djadpfa1002@naver.com>
2022-07-19 01:14:06 +0900

    init

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#"></b-navbar-brand>
+
+    <b-navbar-brand>Tiny IoT</b-navbar-brand>
+
+    <b-collapse id="nav-text-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-text>Resource Browser</b-nav-text>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+    name: "header"
+};
+</script>
